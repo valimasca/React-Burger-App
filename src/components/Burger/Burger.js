cd7ada5687beb05199de7b54a4805151583faa4f
Vali Masca <vali.masca@Vali-Masca-MBP.local>
2020-03-23 13:19:20 -0700

    adding the build control and ingredient suum, connecting state to build controls and removing ingredients safely

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,29 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
+    //igKey = we need a key for each ingredient ig stands for ingredient.
+    //the outside map maps around all ingredients and then returns an array with the spread operator ...Array - which creates an array
+    //we are transforming an object of key value pairs in to an array of burger ingredients where the value of the object is important to how many ingredients I need
+    //Then the key is important for which type of ingredients I need 
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+           return  <BurgerIngredient key={igKey + i} type={igKey} />;
+        });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+    
+    //console.log(transformedIngredients);
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient  type="bread-top" />
-            <BurgerIngredient  type="cheese" />
-            <BurgerIngredient  type="meat" />
+            {transformedIngredients}
             <BurgerIngredient  type="bread-bottom" />
         </div>
     );
