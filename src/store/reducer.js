ce29069c3fc8e33d8b58a23e7a59f9036619cc90
Vali Masca <vali.masca@Vali-Masca-MBP.local>
2020-06-09 08:28:55 -0700

    adding the ingredients in the reducer and connecting the burger builder to our store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,40 @@
 import * as actionTypes from './actions';
 
 const initialState = {
-    ingredients: null,
+    ingredients:{
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 4
 };
 
 const reducer = (state = initialState, action) => {
-
+    switch (action.type){
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                //copy old state with the spread operator and distributing it over the new object below:
+                ...state,
+                //we are setting ingredients to something new, a new object
+                // we are doing it like so that we don't reuse the old state but create a new one
+                ingredients: {
+                    ...state.ingredients,
+                    //[] - this is special sintax to overide a property in a given JS object 
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+            };
+        default: 
+            return state;
+    }
 };
 
 export default reducer;
